bd181a990cea5decd9f09a85152855f604fdb918
eamobi <amobisomto@gmail.com>
2019-02-16 19:38:24 -0600

    setup for smart car api

---
+++ WithSmartCarApi/gettingCall/main.py
@@ -0,0 +1,56 @@
+import smartcar
+from flask import Flask, redirect, request, jsonify
+from flask_cors import CORS
+
+import os
+
+app = Flask(__name__)
+CORS(app)
+
+# global variable to save our access_token
+access = None
+
+client = smartcar.AuthClient(
+    client_id=os.environ.get('CLIENT_ID'),
+    client_secret=os.environ.get('CLIENT_SECRET'),
+    redirect_uri=os.environ.get('REDIRECT_URI'),
+    scope=['read_vehicle_info'],
+    test_mode=True,
+)
+
+
+@app.route('/login', methods=['GET'])
+def login():
+
+
+    auth_url = client.get_auth_url()
+    return redirect(auth_url)
+
+
+@app.route('/exchange', methods=['GET'])
+def exchange():
+    code = request.args.get('code')
+    global access
+    access = client.exchange_code(code)
+    
+    return '', 200
+
+
+
+
+@app.route('/vehicle', methods=['GET'])
+def vehicle():
+    global access
+    vehicle_ids = smartcar.get_vehicle_ids(access['access_token'])['vehicles']
+
+    vehicle = smartcar.Vehicle(vehicle_ids[0], access['access_token'])
+    info = vehicle.info()
+    print(info)
+
+    return jsonify(info)
+
+    pass
+
+
+if __name__ == '__main__':
+    app.run(port=8000)
