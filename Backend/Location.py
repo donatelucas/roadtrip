b690d3f50c0e77c13fe2e5114a236d23f4e78c05
Martin Zugschwert <martin.e.zugschwert@gmail.com>
2019-02-17 08:30:51 -0600

    Commenting out a print statement

--- Backend/Location.py
+++ Backend/Location.py
@@ -9,7 +9,7 @@ def getLatLong(loc_name):
     #gmaps = googlemaps.Client(key=googleAcess.getKey('GoogleKey'))
     gmaps = googlemaps.Client('AIzaSyC4IGZTARjrzzu6VcGBbMdXOPD1Z6hl7so')
     coordinates = gmaps.geocode(loc_name)
-    print(coordinates[0]["formatted_address"])
+    #print(coordinates[0]["formatted_address"])
     return jsonify(name = coordinates[0]["formatted_address"], position = coordinates[0]["geometry"]["location"])
     #return jsonify(name=coordinates)
 
