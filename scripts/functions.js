70fc8b473b65b10fe008fa467cc2d6878e6c4050
Lucas Perez <knightfication@gmail.com>
2019-02-16 23:42:51 -0600

    Added Now Playing update on Now Playing Block

--- scripts/functions.js
+++ scripts/functions.js
@@ -1,9 +1,9 @@
 console.log("SEE YOU SPACE COWBOY...");
 
 var input;
-var playlist;
-var ar = [];
-var arP = [];
+var playlist_input;
+var landmarks = [];
+var playlist = [];
 
 function fetchInput() {
     input = document.getElementById('landmarks').value;
@@ -11,27 +11,37 @@ function fetchInput() {
 }
 
 function fetchPlayList() {
-    playlist = document.getElementById('playlist').value;
+    playlist_input = document.getElementById('playlist').value;
     makePlaylist();
+    setSong();
 }
 
 function makePlaylist(){
-    arP = playlist.split(",");
-    return arP;
+    playlist = playlist_input.split(",");
+    return playlist;
 }
 
 function readInput() {
     return input;
 }
 
-function makeList(){
-    ar = input.split(","); // The split(string) method returns an array
-    return ar;
+function makeList(string){
+    landmarks = input.split(","); // The split(string) method returns an array
+    return landmarks;
 }
 
 function readLists(){
-    for (var i = 0; i < ar.length; i++){
-        console.log(ar[i])
-        console.log(arP[i]);
+    for (var i = 0; i < landmarks.length; i++){
+        console.log(landmarks[i]);
     }
+}
+
+function readPlayList(){
+    for (var i = 0; i < playlist.length; i++){
+        console.log(playlist[i]);
+    }
+}
+
+function setSong(){
+    document.getElementById('now_playing').innerHTML = "Now playing: " + playlist[0];
 }
