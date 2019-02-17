59ed8f23a3328fa27328ef2598530f263c6b6443
Martin Zugschwert <martin.e.zugschwert@gmail.com>
2019-02-17 04:52:23 -0600

    combined the smart car api with the server.py

--- server.py
+++ server.py
@@ -3,10 +3,24 @@ import Location
 import ApiKey
 import MapInterface
 import metricsFromSmartcar
-from flask import Flask, jsonify, request
+import smartcar
+import os
+from flask import Flask, jsonify, request, redirect
 
 
 app = Flask(__name__)
+
+# global variable to save our access_token
+access = None
+
+client = smartcar.AuthClient(
+    client_id='1714bcc1-e189-489f-81f1-aaf39fe89785',
+    client_secret='4d17ecec-e19a-4616-baf3-ca83ad334eae',
+    redirect_uri='http://localhost:5000/exchange',
+    scope=['read_vehicle_info', 'read_odometer'],
+    test_mode=True,
+
+)
 
 if __name__ == "__main__":
     app.run(port=8000)
@@ -54,14 +68,48 @@ def getMap():
     return MapInterface.getMap()
 
 
+@app.route('/login', methods=['GET'])
+def login():
+
+
+    auth_url = client.get_auth_url()
+    return redirect(auth_url)
+
+
+@app.route('/exchange', methods=['GET'])
+def exchange():
+    code = request.args.get('code')
+    global access
+    access = client.exchange_code(code)
+    
+    return '', 200
+
+
+
+@app.route('/vehicle', methods=['GET'])
+def vehicle():
+    global access
+    vehicle_ids = smartcar.get_vehicle_ids(access['access_token'])['vehicles']
+
+    vehicle = smartcar.Vehicle(vehicle_ids[0], access['access_token'])
+    info = vehicle.info()
+    #print(info)
+
+    response = vehicle.odometer()
+    #print(response)
+    
+    return jsonify(info, response)
 
 
 '''
 returns the car metrics with provided options{odometer,location,both} get will return all available
+'''
 '''
 @app.route('/carmetrics', methods = ['GET','POST'])
 def getMetrics():
     
     metricsFromSmartcar.login()
     vInfo, vOdo = metricsFromSmartcar.vehicle()
-    return vInfo, vOdo
+    return str(vInfo), str(vOdo)
+'''
+   
