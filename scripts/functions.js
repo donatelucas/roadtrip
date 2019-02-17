4ec6cf312b0c169f1aad89ffa25417f3de9d3df3
Lucas Perez <knightfication@gmail.com>
2019-02-16 22:52:25 -0600

    Setup now returns an array of landmarks separated by commas

--- scripts/functions.js
+++ scripts/functions.js
@@ -1,11 +1,24 @@
 console.log("See ya Space Cowboy");
 
 var input;
+var ar = [];
 
 function fetchInput() {
     input = document.getElementById('landmarks').value;
+    makeList();
 }
 
 function readInput() {
     return input;
+}
+
+function makeList(){
+    ar = input.split(","); // The split(string) method returns an array
+    return ar;
+}
+
+function readList(){
+    for (var i = 0; i < ar.length; i++){
+        console.log(ar[i]);
+    }
 }
